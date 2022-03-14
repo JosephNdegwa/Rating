1f202c344b15cf9d639bd873fbdfbf11c88cd861
josephndegwa <kariukindegwa98@gmail.com>
2022-03-14 20:19:55 +0300

    update views logic

--- rating/views.py
+++ rating/views.py
@@ -64,3 +64,29 @@ def add_rating(request, project):
     else:
         form = RatingForm()
         return render(request, 'rate-project.html', {'form': form})
+
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('homepage')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST, request.FILES)
+        if form.is_valid():
+            print("handling signup")
+            user = form.save()
+            user.refresh_from_db()
+            user.profile.email = form.cleaned_data.get('email')
+            user.profile.profile_photo = form.cleaned_data.get('profile_photo')
+            user.profile.bio = form.cleaned_data.get('bio')
+            user.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            print("signed up")
+            return redirect('homepage')
+        else:
+            return render(request, 'registration/signup.html', {'form': form})
+    else:
+        form = SignUpForm()
+        return render(request, 'registration/signup.html', {'form': form})
