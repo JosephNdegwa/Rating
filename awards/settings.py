e5a8d1f9eee3a31666e5a7d8013c16c57914c013
josephndegwa <kariukindegwa98@gmail.com>
2022-03-15 16:46:03 +0300

    change paths for adding project

--- awards/settings.py
+++ awards/settings.py
@@ -81,8 +81,10 @@ WSGI_APPLICATION = 'awards.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rating',
+        'USER':'kariuki_ndegwa',
+        'PASSWORD':'515021'
     }
 }
 
